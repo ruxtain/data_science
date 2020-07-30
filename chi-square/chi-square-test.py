8f97487590858759150c71de9f017e975eac0d2b
v_chtan <v_chtan@tencent.com>
2020-07-30 09:54:47 +0800

    卡方检验拟合优度检验

--- chi-square/chi-square-test.py
+++ chi-square/chi-square-test.py
@@ -6,9 +6,7 @@ $\\Chi^2$ 这个是希腊字母Chi，看起来像大写的X。
 如果有一个变量Z服从标准正态分布，那么Z^2符合自由度为1的卡方分布
 """ 
 
-# from scipy.stats import chisquare
-# import numpy as np
-
+from scipy.stats import chisquare
 
 observed = [5, 82, 251] 
 expected = [13.52, 108.16, 216.32]
@@ -26,3 +24,15 @@ def get_chisquare_statistic(observed, expected):
 
 X2 = get_chisquare_statistic(observed, expected)
 print(X2)
+
+# 本来打算传入X2后计算p-value的，但是这个函数很直接，传入原始
+output = chisquare(observed, expected)
+print(output.statistic, output.pvalue)
+
+
+# 如果根据数据推断均值，则损失一个额外的自由度，因此 ddof = 1
+# 自由度 = number of cells - 1 - ddof
+observed = [5, 82, 251] 
+expected = [6.26, 79.48, 252.26]
+output = chisquare(observed, expected, ddof=1)
+print(output.statistic, output.pvalue)
